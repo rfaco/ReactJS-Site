40cea76ac1d52846d3a86641dcd2b668a75dce09
Roger Faco <rfacodev@gmail.com>
2021-02-21 16:24:42 -0300

    hero components

--- reactjs-website/src/App.js
+++ reactjs-website/src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/pages/Home';
+
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact />
-      </Switch>
-    </Router>
+      <Router>
+        <Navbar />
+        <Switch>
+
+          <Route path='/' exact component={Home}/>
+        </Switch>
+      </Router>
     </>
   );
 }
