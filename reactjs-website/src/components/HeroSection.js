d7be406a6b46090ba3f18a012213c2458a431830
Roger Faco <rfacodev@gmail.com>
2021-02-21 16:39:36 -0300

    video hero + menu specs

--- reactjs-website/src/components/HeroSection.js
+++ reactjs-website/src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      <video src='/videos/office-test.mp4' autoPlay loop muted />
       <h1>INDA DIGITAL</h1>
       <p>Embarque conosco!</p>
       <div className='hero-btns'>
